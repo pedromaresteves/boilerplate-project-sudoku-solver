23279cbddde4a1e09debde42014951c7523917a1
Pedro Esteves <pedromaresteves@gmail.com>
2024-09-24 22:13:13 +0100

    Add validation methods

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,26 +1,80 @@
 class SudokuSolver {
 
   validate(puzzleString) {
-    const regex = /[^1-9\.]/g;
+    const invalidCharsRegex = /[^1-9\.]/g;
     if (puzzleString.length !== 81) return false
-    if (puzzleString.match(regex)) return false
+    if (puzzleString.match(invalidCharsRegex)) return false
     return true
   }
 
-  checkRowPlacement(puzzleString, row, column, value) {
-
+  checkRowPlacement(puzzleString, row, value) {
+    if (!row || typeof row !== 'string') return false
+    const rowLetterConvertedToNumber = (row.charCodeAt(0) - 65);
+    const puzzleStringRows = [[]];
+    let puzzleRow = 0;
+    puzzleString.split("").forEach((item, index) => {
+      if (index > 1 && index % 9 === 0) {
+        puzzleStringRows.push([])
+        puzzleRow++;
+      }
+      puzzleStringRows[puzzleRow].push(item);
+    })
+    if (!puzzleStringRows[rowLetterConvertedToNumber]) return false;
+    return !puzzleStringRows[rowLetterConvertedToNumber].includes(value);
   }
 
-  checkColPlacement(puzzleString, row, column, value) {
-
+  checkColPlacement(puzzleString, column, value) {
+    if (!column) return false
+    const puzzleStringColumns = [[]];
+    let arrayIndex = 0;
+    puzzleString.split("").forEach((item) => {
+      if (!puzzleStringColumns[arrayIndex]) puzzleStringColumns.push([])
+      puzzleStringColumns[arrayIndex].push(item)
+      arrayIndex++;
+      if (arrayIndex === 9) arrayIndex = 0
+    });
+    return !puzzleStringColumns[column - 1].includes(value);
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
-
+    const puzzleStringSquares = [[]];
+    const puzzleStringed = puzzleString.split("");
+    const region = calculateRegion(row, column);
+    let puzzleZone = 0;
+    for (let i = 0; i < puzzleStringed.length; i++) {
+      if (i > 1 && i % 3 === 0) {
+        puzzleZone++
+      }
+      if (i > 1 && i % 9 === 0 && i % 27 !== 0) {
+        puzzleZone -= 3;
+      }
+      if (!puzzleStringSquares[puzzleZone]) puzzleStringSquares[puzzleZone] = []
+      puzzleStringSquares[puzzleZone].push(puzzleStringed[i])
+    }
+    return !puzzleStringSquares[region].includes(value);
   }
 
   solve(puzzleString) {
   }
+}
+
+const calculateRegion = (row, column) => {
+  const rowLetterConvertedToNumber = (row.charCodeAt(0) - 65);
+  const regionBlocksByColumn = [[0, 3, 6], [1, 4, 7], [2, 5, 8]];
+  let columnBlock = 0;
+  let rowBlock = 0;
+  for (let i = 0; i < column; i++) {
+    if (i > 1 && i % 3 === 0) {
+      columnBlock++
+    }
+  }
+
+  for (let i = 0; i <= rowLetterConvertedToNumber; i++) {
+    if (i > 1 && i % 3 === 0) {
+      rowBlock++
+    }
+  }
+  return regionBlocksByColumn[columnBlock][rowBlock]
 }
 
 module.exports = SudokuSolver;
--- routes/api.js
+++ routes/api.js
@@ -8,11 +8,32 @@ module.exports = function (app) {
 
   app.route('/api/check')
     .post((req, res) => {
-
+      const validationData = { valid: true }
+      const conflicts = [];
+      const { puzzle, coordinate, value } = req.body;
+      const isValueValidForSelectedRow = solver.checkRowPlacement(puzzle, coordinate[0], value);
+      const isValueValidForSelectedColumn = solver.checkColPlacement(puzzle, coordinate[1], value);
+      const IsValueValidForSquare = solver.checkRegionPlacement(puzzle, coordinate[0], coordinate[1], value);
+      if (!value && !coordinate) return res.send({ error: "Required field(s) missing" })
+      if (!isValueValidForSelectedRow) {
+        validationData.valid = false;
+        conflicts.push("row")
+      }
+      if (!isValueValidForSelectedColumn) {
+        validationData.valid = false;
+        conflicts.push("column")
+      }
+      if (!IsValueValidForSquare) {
+        validationData.valid = false;
+        conflicts.push("region")
+      }
+      if (conflicts.length) validationData.conflict = conflicts;
+      res.send(validationData)
     });
 
   app.route('/api/solve')
     .post((req, res) => {
-      console.log(solver.validate(req.body.puzzle))
+      const isValidated = solver.validate(req.body.puzzle)
+      console.log(isValidated)
     });
 };
